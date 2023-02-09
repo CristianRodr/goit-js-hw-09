91d4501c0f0aa397dbe0b35db019e5e8de9d9a1d
cristianRodri <an1me@hotmail.es>
2023-02-09 08:26:57 -0500

    color-switcher - completado ^_~

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let timerColor = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function crearStyle() {
+  return body.style.backgroundColor = getRandomHexColor();
+}
+
+function ocultarBtn(ocultar) {
+  ocultar.addEventListener('click', (event) => {
+    if (event.target === btnStart) {
+      btnStart.disabled = true;
+      btnStop.disabled = false;
+    } else if (event.target === btnStop) {
+      btnStart.disabled = false;
+      btnStop.disabled = true;
+    }
+  });
+}
+
+const onClik = () => {
+  timerColor = setInterval(() => {
+    crearStyle();
+  }, 1000);
+};
+
+const ofClik = () => {
+  clearInterval(timerColor);
+};
+
+btnStart.addEventListener('click', onClik);
+
+btnStop.addEventListener('click', ofClik);
+
+ocultarBtn(body);
